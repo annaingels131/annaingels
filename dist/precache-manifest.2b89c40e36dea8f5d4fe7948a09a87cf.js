6df3ab4ac17d0b819ee7fa277f53d59d5a09536a
Anna Ingels <annaingels131@yahoo.com>
2018-09-23 11:24:42 -0700

    fixnging error

---
+++ dist/precache-manifest.2b89c40e36dea8f5d4fe7948a09a87cf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ae4820b51a760af9e3da7c96337fb4",
+    "url": "/img/hey.35ae4820.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "73917960bc9cb44db39501b75165a2cf",
+    "url": "/img/ordr.73917960.png"
+  },
+  {
+    "revision": "393a658684d6a6256fcd",
+    "url": "/css/chunk-vendors.3b6122ca.css"
+  },
+  {
+    "revision": "323de5d72ae81daac146de54303d6f00",
+    "url": "/img/socks.323de5d7.png"
+  },
+  {
+    "revision": "d3885ff4f05fb8caae959a228b17fa40",
+    "url": "/img/hidden.d3885ff4.png"
+  },
+  {
+    "revision": "fde2bc5eea8f021004a21703b51e1262",
+    "url": "/img/Lupita.fde2bc5e.png"
+  },
+  {
+    "revision": "2f50ebd5d40c42a34974eee04312a417",
+    "url": "/img/bb.2f50ebd5.png"
+  },
+  {
+    "revision": "05853dbd668a2c016f6d8906917fecb5",
+    "url": "/img/annaplants.05853dbd.png"
+  },
+  {
+    "revision": "b055cf42f12af7ea1384ffaefc9a2b0a",
+    "url": "/img/flower.b055cf42.png"
+  },
+  {
+    "revision": "e846a6a63f155806851d94d8057d5167",
+    "url": "/img/Anna.e846a6a6.png"
+  },
+  {
+    "revision": "e88dcd247d3ef94836b1",
+    "url": "/js/app.84471b53.js"
+  },
+  {
+    "revision": "393a658684d6a6256fcd",
+    "url": "/js/chunk-vendors.92c74a26.js"
+  },
+  {
+    "revision": "987a0bc2a22f6f9f776c2f22b96aee82",
+    "url": "/img/ephone.987a0bc2.png"
+  },
+  {
+    "revision": "a7cb798624a7cfa16e52f4b95a607b86",
+    "url": "/img/annaingels.a7cb7986.jpg"
+  },
+  {
+    "revision": "46bfef9e001fcf0efffd64730db8e706",
+    "url": "/img/leaf.46bfef9e.png"
+  },
+  {
+    "revision": "9a9bfdfb1508f22786e2f30b9e4de2b0",
+    "url": "/img/elogo.9a9bfdfb.png"
+  },
+  {
+    "revision": "40744d8ab5604873811de01243248685",
+    "url": "/img/appstore.40744d8a.png"
+  },
+  {
+    "revision": "9ca112781c5c1ee1f19c4ebbc388bd99",
+    "url": "/img/god.9ca11278.png"
+  },
+  {
+    "revision": "5eccde900646ee1f47d3571e989346de",
+    "url": "/img/ordrslip.5eccde90.png"
+  },
+  {
+    "revision": "f2ce0f8e6a4fc45c6e0a07f237d64ea3",
+    "url": "/img/brit.f2ce0f8e.png"
+  },
+  {
+    "revision": "c5d6a02cfa7640bc11467a628fe4ecf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88dcd247d3ef94836b1",
+    "url": "/css/app.482d3e02.css"
+  }
+];
